62ffbfb2ee3d50f2461467001e4ef0ed0b434e77
Thiago <thiago.nogueira187@gmail.com>
2021-08-25 22:56:03 -0400

    fix: Adicionado cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+
 const routes = require('./routes')
 
 const app = express()
 
 // parse application/json
 app.use(express.json())
+app.use(cors())
 
 // register routes
 routes(app)
